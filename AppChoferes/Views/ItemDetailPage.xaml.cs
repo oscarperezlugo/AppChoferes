25facf0e19214fab727e5fb54e7dfc918a91c8c1
oscarperezlugo <56516855+oscarperezlugo@users.noreply.github.com>
2021-06-14 12:42:20 -0400

    Initial commit

---
+++ AppChoferes/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using AppChoferes.ViewModels;
+
+namespace AppChoferes.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
